b74e85cd8c738b557f61f2dbeb882201ec746a3a
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2020-05-23 05:21:51 -0400

    added storage bucket url to admin initialize

--- functions/utils/admin.js
+++ functions/utils/admin.js
@@ -2,7 +2,8 @@ const admin = require("firebase-admin");
 var serviceAccount = require("../cookwithme-fc78a-firebase-adminsdk-ulj6j-3ed75b7b32.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://cookwithme-fc78a.firebaseio.com"
+  databaseURL: "https://cookwithme-fc78a.firebaseio.com",
+  storageBucket: "cookwithme-fc78a.appspot.com"
 });
 
 const db = admin.firestore();
